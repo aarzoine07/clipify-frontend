259a8eb5a55b2d0ca50e40cea92aa062e85686b1
aarzoine07 <aarzoine@gmail.com>
2025-09-12 13:23:18 -0400

    short message about what you did

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+
 export default function AccountPage() {
   return (
     <div className="space-y-4">
